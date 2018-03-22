4c1d3763950bcf4c728109177f8205eca801dd54
deetma01 <36896421+deetma01@users.noreply.github.com>
2018-03-21 20:32:00 -0500

    Add files via upload

--- controller.js
+++ controller.js
@@ -1,31 +1,14 @@
-var stores = ['Fareway', 'Ace Hardware', 'Caseys', 'The Hatchery', 'Amundsens']
-var sections = ['Produce', 'Meats', 'Cereal', 'Canned Goods', 'Frozen Foods', 'Dairy', 'Liquor', 'Tools', 'Clothing']
 
-var shoppingModel = new ShoppingList()
-var myView = new ShoppingView(shoppingModel)
+var shoppinglist = new ShoppingList()
+var view = new View(shoppinglist)
+myDB = new LocalStorageSaver(shoppinglist, "matts_storage")
 
 function clickedon() {
     let rowcolids = ['itemname', 'qty', 'store', 'category', 'price', 'priority']
     let vals = {}
-    for (let cid of rowcolids) {
-        vals[cid] = document.getElementById(cid).value;
+    for (let id of rowcolids) {
+        vals[id] = document.getElementById(id).value;
     }
-    let it = new Item(vals.itemname, vals.qty, vals.priority, vals.store, vals.category, vals.price)
-    shoppingModel.addItem(it)
+    let item = new Item(vals.itemname, vals.qty, vals.priority, vals.store, vals.category, vals.price)
+    shoppinglist.addItem(item)
 }
-
-
-function populateSelect(selectId, sList) {
-    let sel = document.getElementById(selectId, sList)
-    for (let s of sList) {
-        let opt = document.createElement("option")
-        opt.value = s
-        opt.innerHTML = s
-        sel.appendChild(opt)
-    }
-}
-
-$(document).ready(function () {
-    populateSelect('store', stores)
-    populateSelect('category', sections)
-})
--- model.js
+++ model.js
@@ -1,32 +1,6 @@
 'use strict';
-class Item {
-    constructor(name, quantity, priority, store, section, price) {
-        this.name = name;
-        this.quantity = quantity;
-        this.priority = priority;
-        this.store = store;
-        this.section = section;
-        this.price = price;
-
-        this._purchased = false;
-
-    }
-
-    get purchased() {
-        return this._purchased;
-    }
-
-    set purchased(nv) {
-        this._purchased = nv;
-        alert(`${this.name} was purchased`)
-    }
-
-
-
-}
-
 class Subject {
-
+ 
     constructor() {
         this.handlers = []
     }
@@ -34,7 +8,7 @@ class Subject {
     subscribe(fn) {
             this.handlers.push(fn);
         }
-
+     
     unsubscribe(fn) {
         this.handlers = this.handlers.filter(
             function(item) {
@@ -44,7 +18,7 @@ class Subject {
             }
         );
     }
-
+     
     publish(msg, someobj) {
         var scope = someobj || window;
         for (let fn of this.handlers) {
@@ -53,16 +27,60 @@ class Subject {
     }
 }
 
+class Item extends Subject {
+    constructor(name, quantity, priority, store, section, price) {
+        super()
+        this.name = name;
+        this.quantity = quantity;
+        this.priority = priority;
+        this.store = store;
+        this.section = section;
+        this.price = price;
+        this._purchased = false;
+    }
+
+    get purchased() {
+        return this._purchased;
+    }
+    
+    set purchased(nv) {
+        if(this._purchased == false){
+            this._purchased = nv
+            this.publish('removed item',this)
+        }
+        else {
+            this._purchased = false
+            clearTimeout(this.to)
+            this.publish('added',this)
+        }
+    }
+}
 
 class ShoppingList extends Subject {
     constructor() {
         super()
-        this.newItems = []
-        this.oldItems = [];
+        this.Items = [];
     }
 
     addItem(it) {
-        this.newItems.push(it)
+        this.Items.push(it);
+        let self = this;
+        it.subscribe(function(a,b) {
+            self.publish('removed_start', self)
+            if(it.purchased == true) {
+                it.to = setTimeout(function() {
+                    self.removeItem(it);
+                }, 2000)
+            }
+        });
         this.publish('newitem', this)
+    }
+
+    removeItem(it) {
+        let idx = this.Items.indexOf(it);
+        if(idx > -1) {
+            let it = this.Items.splice(idx, 1)
+        }
+        this.publish('removed_final', this)
     }
 }
